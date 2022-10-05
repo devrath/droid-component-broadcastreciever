a8a066d236bc4bf9b120cdb793080f8e33ab730b
Devrath <devrath@mplgaming.com>
2022-10-05 14:09:45 +0530

    added new code

--- Application/app/src/main/java/com/droid/code/demos/local_broadcast/LocalBroadcastActivity.kt
+++ Application/app/src/main/java/com/droid/code/demos/local_broadcast/LocalBroadcastActivity.kt
@@ -7,14 +7,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.droid.code.databinding.ActivityLocalBroadcastBinding
 import com.droid.code.demos.local_broadcast.reciever.CUSTOM_ACTION
-import com.droid.code.demos.local_broadcast.reciever.CustomLocalBroadcastReciever
+import com.droid.code.demos.local_broadcast.reciever.CustomLocalBroadcastReceiver
 
 
 class LocalBroadcastActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLocalBroadcastBinding
 
-    private var receiver : CustomLocalBroadcastReciever = CustomLocalBroadcastReciever()
+    private var receiver : CustomLocalBroadcastReceiver = CustomLocalBroadcastReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- Application/app/src/main/java/com/droid/code/demos/local_broadcast/reciever/CustomLocalBroadcastReceiver.kt
+++ Application/app/src/main/java/com/droid/code/demos/local_broadcast/reciever/CustomLocalBroadcastReceiver.kt
@@ -3,12 +3,11 @@ package com.droid.code.demos.local_broadcast.reciever
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import android.widget.Toast
 
 const val CUSTOM_ACTION = "custom-action-local-broadcast"
 
-class CustomLocalBroadcastReciever : BroadcastReceiver() {
+class CustomLocalBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent) {
         val data = intent.getStringExtra("message")
